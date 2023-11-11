04eb9f924abfbbd860ec104fa12fffee7f8cbc97
Harut20024 <htarzyanh@gmail.com>
2023-11-11 12:20:25 +0400

    adding bot and animation

--- classes/Card.js
+++ classes/Card.js
@@ -1,40 +1,40 @@
 class Card {
-  constructor(suit, value, img, x, y) {
-      this.suit = suit;
-      this.value = value;
-      this.img = img;
-      this.x = x;
-      this.y = y;
-      this.startX = x; // Starting x position
-      this.startY = y; // Starting y position
-      this.dragging = false;
-      this.width = 70; // Width of the card image
-      this.height = 100; // Height of the card image
-  }
+    constructor(suit, value, img, x, y) {
+        this.suit = suit;
+        this.value = value;
+        this.img = img;
+        this.x = x;
+        this.y = y;
+        this.startX = x;
+        this.startY = y;
+        this.dragging = false;
+        this.width = 100;
+        this.height = 130;
+    }
 
-  display() {
-      if (this.dragging) {
-          this.x = mouseX - this.width / 2;
-          this.y = mouseY - this.height / 2;
-      }
-      image(this.img, this.x, this.y, this.width, this.height);
-  }
+    display() {
+        if (this.dragging) {
+            this.x = mouseX - this.width / 2;
+            this.y = mouseY - this.height / 2;
+        }
+        image(this.img, this.x, this.y, this.width, this.height);
+    }
 
-  isMouseOver() {
-      return mouseX > this.x && mouseX < this.x + this.width &&
-             mouseY > this.y && mouseY < this.y + this.height;
-  }
+    isMouseOver() {
+        return mouseX > this.x && mouseX < this.x + this.width &&
+            mouseY > this.y && mouseY < this.y + this.height;
+    }
 
-  startDragging() {
-      this.dragging = true;
-  }
+    startDragging() {
+        this.dragging = true;
+    }
 
-  stopDragging() {
-      this.dragging = false;
-  }
+    stopDragging() {
+        this.dragging = false;
+    }
 
-  resetPosition() {
-      this.x = this.startX;
-      this.y = this.startY;
-  }
+    resetPosition() {
+        this.x = this.startX;
+        this.y = this.startY;
+    }
 }
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-let displayedCards = [];
+let movingCard = null; // Global variable to track the moving card
+let playerCards = [];
+let botCards = []
 let fullDeck = [];
+let ImgCalodes;
+let turn = "player";
+
+function switchTurn() {
+    turn = (turn === "player") ? "bot" : "player";
+}
 
 function preload() {
+    ImgCalodes = loadImage(`Images/caloda.png`);
     const suitsImages = {
         'Clubs': [],
         'Diamonds': [],
@@ -24,38 +33,113 @@ function preload() {
 
 
 function setup() {
-    createCanvas(800, 700);
+    createCanvas(900, 800);
     shuffleArray(fullDeck);
 
-    // Set up the displayed cards
-    displayedCards = fullDeck.slice(0, 6);
-    let startX = (width - (displayedCards.length * 110)) / 2;
-    let startY = height - 150;
+    playerCards = [];
+    botCards = [];
 
-    // Position the displayed cards at the bottom of the canvas
-    for (let i = 0; i < displayedCards.length; i++) {
-        displayedCards[i].x = startX + i * 110;
-        displayedCards[i].y = startY;
-        displayedCards[i].startX = displayedCards[i].x; // Set the starting position for reset
-        displayedCards[i].startY = displayedCards[i].y;
+    for (let i = 0; i < 6; i++) {
+        playerCards.push(fullDeck.pop());
+        botCards.push(fullDeck.pop());
     }
+
+    // Sorting by suit and then by value
+    playerCards.sort((a, b) => {
+        if (a.suit === b.suit) {
+            return a.value - b.value;
+        }
+        return a.suit.localeCompare(b.suit);
+    });
+
+    botCards.sort((a, b) => {
+        if (a.suit === b.suit) {
+            return a.value - b.value;
+        }
+        return a.suit.localeCompare(b.suit);
+    });
+
+    dealCards();
 }
+
+
+
+
+
 
 function draw() {
     background(0, 128, 0);
-    for (let card of displayedCards) {
+    displayDeckCount();
+    displayTurnIndicator(); // New function to display the current turn
+    if (movingCard) {
+        let progress = movingCard.progress;
+        movingCard.card.x = lerp(movingCard.startX, movingCard.targetX, progress);
+        movingCard.card.y = lerp(movingCard.startY, movingCard.targetY, progress);
+
+        // Update the progress
+        movingCard.progress += 0.05; // Adjust speed as needed
+
+        // Check if the animation is complete
+        if (movingCard.progress >= 1) {
+            movingCard = null;
+        }
+    }
+    if (movingCard) {
+        movingCard.card.display(); // Display the moving card on top
+    }
+    for (let card of playerCards) {
+        card.display();
+    }
+    for (let card of botCards) {
         card.display();
     }
 }
 
+function displayTurnIndicator() {
+    fill(255);
+    textSize(24);
+    text(`Current Turn: ${turn}`, width / 2, 30);
+}
+
+
 function mousePressed() {
-    for (let card of displayedCards) {
+    if (turn !== "player") return;
+
+    for (let card of playerCards) {
         if (card.isMouseOver()) {
             card.startDragging();
             break;
         }
     }
 }
+
+function mouseReleased() {
+    if (turn !== "player") return;
+    let playerCardToBot;
+
+    for (let card of playerCards) {
+        if (card.dragging) {
+            card.stopDragging();
+
+            if (isInCenter(card)) {
+                if (placeCardInCenter(card)) {
+                    playerCardToBot = card;
+                    switchTurn();
+                    break;
+                }
+            } else {
+                card.resetPosition();
+            }
+        }
+    }
+
+    // Call botRespondToAttack only if a card was placed in the center
+    if (playerCardToBot) {
+        botRespondToAttack(playerCardToBot);
+    }
+}
+
+
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
@@ -65,22 +149,6 @@ function shuffleArray(array) {
 }
 
 
-function mouseReleased() {
-    for (let card of displayedCards) {
-        if (card.dragging) {
-            card.stopDragging();
-
-            if (isInCenter(card)) {
-                // Attempt to place the card in the center, if successful, don't reset the position
-                if (placeCardInCenter(card)) {
-                    return;
-                }
-            }
-            // If the card wasn't placed in the center, reset its position
-            card.resetPosition();
-        }
-    }
-}
 
 function placeCardInCenter(droppedCard) {
     const centerPositions = calculateCenterPositions();
@@ -94,21 +162,19 @@ function placeCardInCenter(droppedCard) {
             return true;
         }
     }
-    // If no available position, the card can't be placed
     return false;
 }
 
 function isPositionEmpty(position, tolerance = 10) {
-    return !displayedCards.some(card => {
+    return !playerCards.some(card => {
         return abs(card.x - position.x) < tolerance && abs(card.y - position.y) < tolerance;
     });
 }
 
 function calculateCenterPositions() {
-    // Calculate the positions for up to 6 cards in the center
     const positions = [];
-    const cardWidth = 70; // Assuming all cards have the same width
-    const startX = (width / 2) - (3 * cardWidth); // Start placing cards from the left of center
+    const cardWidth = 105; // Assuming all cards have the same width
+    const startX = (width / 2) - (3 * cardWidth) + 50; // Start placing cards from the left of center
 
     for (let i = 0; i < 6; i++) {
         positions.push({
@@ -119,14 +185,99 @@ function calculateCenterPositions() {
     return positions;
 }
 
-function isInCenter(card, tolerance = 100) {
+function isInCenter(card, tolerance = 400) {
     let centerX = width / 2;
     let centerY = height / 2;
-    let cardCenterX = card.x + card.width / 2;
-    let cardCenterY = card.y + card.height / 2;
+    let cardCenterX = card.x + card.width / 2 + 100;
+    let cardCenterY = card.y + card.height / 2 + 100;
 
     // Check if the card's center is within the tolerance range of the canvas's center
     return abs(cardCenterX - centerX) < tolerance && abs(cardCenterY - centerY) < tolerance;
 }
 
+function displayDeckCount() {
+    let count = fullDeck.length
+    let imgX = 10;
+    let imgY = height / 2 - ImgCalodes.height / 2 + 200;
+
+    image(ImgCalodes, imgX, imgY, 120, 120);
+    fill(255);
+    noStroke();
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    text(count, imgX + 60, imgY + 140);
+}
+
+
+function dealCards() {
+    let startX = (width - (playerCards.length * 110)) / 2;
+    let startY = height - 150;
+
+    // Position the player's cards at the bottom of the canvas
+    for (let i = 0; i < playerCards.length; i++) {
+        playerCards[i].x = startX + i * 110;
+        playerCards[i].y = startY;
+        playerCards[i].startX = playerCards[i].x;
+        playerCards[i].startY = playerCards[i].y;
+    }
+
+    let botStartX = (width - (botCards.length * 110)) / 2;
+    let botStartY = 100; // Adjust as needed for visibility
+
+    // Position the bot's cards at the top of the canvas
+    for (let i = 0; i < botCards.length; i++) {
+        botCards[i].x = botStartX + i * 110;
+        botCards[i].y = botStartY;
+        botCards[i].botStartX = botCards[i].x;
+        botCards[i].botStartY = botCards[i].y;
+    }
+
+}
+
+
+
+
+//bot
+
+
+function botRespondToAttack(playerCard) {
+    // Try to find a card of the same suit with a higher value
+    let responseCard = botCards.find(card => card.suit === playerCard.suit && card.value > playerCard.value);
+
+    // // If no card found and player's card is not a trump, try to beat with a trump card
+    // if (!responseCard && playerCard.suit !== trumpSuit) {
+    //     responseCard = botCards.find(card => card.suit === trumpSuit);
+    // }
+
+    if (responseCard) {
+        botCards = botCards.filter(card => card !== responseCard);
+        playBotCard(responseCard);
+    } else {
+        // Bot cannot respond, picks up the card (or any other rules specific action)
+        // botPickUpCard(playerCard);
+    }
+
+    // Switch turn back to the player
+    turn = "player";
+}
+
+function playBotCard(card) {
+    // Set the target position for the card
+    let targetX = width / 2;
+    let targetY = height / 2;
+
+    // Initialize movingCard with card details and target position
+    movingCard = {
+        card: card,
+        startX: card.x,
+        startY: card.y,
+        targetX: targetX,
+        targetY: targetY,
+        progress: 0 // Progress of the animation, from 0 to 1
+    };
+
+    card.inPlay = true;
+
+    botCards = botCards.filter(c => c !== card);
+}
 
